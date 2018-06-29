8febb284f58b877577fcd8fdd2b6281630f5ecc9
Liam Dawson <liam@ldaws.com>
2018-06-29 23:07:30 +1000

    align with markdown sample

--- style.py
+++ style.py
@@ -14,11 +14,15 @@ class NightOwl(Style):
     Token.Keyword.Reserved: '',
     Token.Keyword.Type: 'noitalic #82aaff',
     Token.Name: '#d6deeb',
+    Token.Name.Attribute: '#ff869a',
     Token.Name.Function: '#82aaff',
     Token.String.Quote: '#d9f5dd',
     Token.String: '#ecc48d',
     Token.Number: '#f78c6c',
     Token.Operator: '#7fdbca',
     Token.Comment: 'italic #637777',
-    Token.Comment.Preproc: 'noitalic #7fdbca'
+    Token.Comment.Preproc: 'noitalic #7fdbca',
+    Token.Generic.Strong: 'bold #addb67',
+    Token.Generic.Emph: 'italic #697098',
+    Token.Generic.Heading: '#82b1ff'
   }
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from style import NightOwl
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
-from pygments.lexers import CLexer
+from pygments.lexers import CLexer, MarkdownLexer
 
 if __name__ == "__main__":
   samples = {
-    "samples/sample.c": CLexer()
+    "samples/sample.c": CLexer(),
+    "samples/sample.md": MarkdownLexer()
   }
 
   formatter = HtmlFormatter(style=NightOwl)
@@ -14,7 +15,11 @@ if __name__ == "__main__":
   with open('test.html', 'w') as out_file:
     out_file.truncate()
     out_file.write("<html><head><link rel='stylesheet' href='base.css'><style>{}</style></head><body>".format(formatter.get_style_defs('div.highlight pre')))
-    for (key, val) in samples.iteritems():
+
+    files = samples.keys()
+    files.sort()
+
+    for key in files:
       with open(key, 'r') as sample_file:
-        out_file.write(highlight(sample_file.read(), val, formatter))
+        out_file.write(highlight(sample_file.read(), samples[key], formatter))
     out_file.write("</body></html>")
