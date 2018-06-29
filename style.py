6218ca7ad0a02c8c40c52391dbc9c289fd40fa7b
Liam Dawson <liam@ldaws.com>
2018-06-29 22:56:35 +1000

    match c sample as closely as possible

--- style.py
+++ style.py
@@ -1,4 +1,24 @@
 from pygments.style import Style
+from pygments.token import Token
 
 class NightOwl(Style):
+  background_color = '#011627'
   default_style = ''
+  styles = {
+    Token: '#d6deeb',
+    Token.Keyword: 'italic #c792ea',
+    Token.Keyword.Constant: 'noitalic #82aaff',
+    Token.Keyword.Declaration: '',
+    Token.Keyword.Namespace: '',
+    Token.Keyword.Psuedo: '',
+    Token.Keyword.Reserved: '',
+    Token.Keyword.Type: 'noitalic #82aaff',
+    Token.Name: '#d6deeb',
+    Token.Name.Function: '#82aaff',
+    Token.String.Quote: '#d9f5dd',
+    Token.String: '#ecc48d',
+    Token.Number: '#f78c6c',
+    Token.Operator: '#7fdbca',
+    Token.Comment: 'italic #637777',
+    Token.Comment.Preproc: 'noitalic #7fdbca'
+  }
--- test.py
+++ test.py
@@ -9,13 +9,12 @@ if __name__ == "__main__":
   }
 
   formatter = HtmlFormatter(style=NightOwl)
+  formatter.style.background_color = '#011627'
 
   with open('test.html', 'w') as out_file:
     out_file.truncate()
+    out_file.write("<html><head><link rel='stylesheet' href='base.css'><style>{}</style></head><body>".format(formatter.get_style_defs('div.highlight pre')))
     for (key, val) in samples.iteritems():
       with open(key, 'r') as sample_file:
         out_file.write(highlight(sample_file.read(), val, formatter))
-
-
-
-
+    out_file.write("</body></html>")
