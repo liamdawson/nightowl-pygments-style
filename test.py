739551bbb1acf9a6c6937c9a45e339ab61ea2b3f
Liam Dawson <liam@ldaws.com>
2018-07-03 21:42:21 +1000

    add pipenv install to travis

--- test.py
+++ test.py
@@ -4,22 +4,24 @@ from pygments import highlight
 from pygments.lexers import CLexer, MarkdownLexer
 
 if __name__ == "__main__":
-  samples = {
-    "samples/sample.c": CLexer(),
-    "samples/sample.md": MarkdownLexer()
-  }
+    samples = {
+        "samples/sample.c": CLexer(),
+        "samples/sample.md": MarkdownLexer()
+    }
 
-  formatter = HtmlFormatter(style=NightOwl)
-  formatter.style.background_color = '#011627'
+    formatter = HtmlFormatter(style=NightOwl)
+    formatter.style.background_color = '#011627'
 
-  with open('test.html', 'w') as out_file:
-    out_file.truncate()
-    out_file.write("<html><head><link rel='stylesheet' href='base.css'><style>{}</style></head><body>".format(formatter.get_style_defs('div.highlight pre')))
+    with open('test.html', 'w') as out_file:
+        out_file.truncate()
+        out_file.write("<html><head><link rel='stylesheet' href='base.css'><style>{}</style></head><body>".format(
+            formatter.get_style_defs('div.highlight pre')))
 
-    files = samples.keys()
-    files.sort()
+        files = samples.keys()
+        files.sort()
 
-    for key in files:
-      with open(key, 'r') as sample_file:
-        out_file.write(highlight(sample_file.read(), samples[key], formatter))
-    out_file.write("</body></html>")
+        for key in files:
+            with open(key, 'r') as sample_file:
+                out_file.write(
+                    highlight(sample_file.read(), samples[key], formatter))
+        out_file.write("</body></html>")
--- to_css.py
+++ to_css.py
@@ -4,6 +4,6 @@ from style import NightOwl
 from pygments.formatters.html import HtmlFormatter
 
 if __name__ == '__main__':
-  html = HtmlFormatter(style=NightOwl)
-  html.style.background_color = '#011627'
-  print(html.get_style_defs("pre.highlight"))
+    html = HtmlFormatter(style=NightOwl)
+    html.style.background_color = '#011627'
+    print(html.get_style_defs("pre.highlight"))
